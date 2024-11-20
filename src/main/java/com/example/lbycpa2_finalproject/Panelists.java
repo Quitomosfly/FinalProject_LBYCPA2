49f7fe159d3817fe7a7042c720e3bc79cdb378cf
Quitomos <paul_marquez@dlsu.edu.ph>
2024-11-20 20:09:00 +0800

    Push

---
+++ src/main/java/com/example/lbycpa2_finalproject/Panelists.java
@@ -0,0 +1,20 @@
+package com.example.lbycpa2_finalproject;
+
+public class Panelists {
+    private String panelistName;
+    private int[] mondayAvailability;
+    private int[] tuesdayAvailability;
+    private int[] wednesdayAvailability;
+    private int[] thursdayAvailability;
+    private int[] fridayAvailability;
+    private int[] saturdayAvailability;
+    private int[] sundayAvailability;
+
+    public Panelists(String panelistName, int[] mondayAvailability, int[] tuesdayAvailibility, int[] wednesdayAvailability, int[] thursdayAvailability, int[] fridayAvailability, int[] saturdayAvailability, int[] sundayAvailability) {
+
+    }
+
+
+
+}
+
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.lbycpa2_finalproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.lbycpa2_finalproject to javafx.fxml;
+    exports com.example.lbycpa2_finalproject;
+}
