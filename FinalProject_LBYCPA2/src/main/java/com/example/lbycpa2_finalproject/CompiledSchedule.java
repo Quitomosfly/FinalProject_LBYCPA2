8bc699dd38f4471e35396923b31975bedf70e7c2
Quitomosfly <134378365+Quitomosfly@users.noreply.github.com>
2024-11-20 20:06:30 +0800

    Add files via upload

---
+++ FinalProject_LBYCPA2/src/main/java/com/example/lbycpa2_finalproject/CompiledSchedule.java
@@ -0,0 +1,11 @@
+package com.example.lbycpa2_finalproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+public class CompiledSchedule {
+
+    //private List<Panelists>
+    @FXML
+    private ComboBox<Panelists> panelistsComboBox;
+}
